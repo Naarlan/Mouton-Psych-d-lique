e138e3625df0552ef764a6a1d210a680915625b1
Naarlan <naarlan.tegeirian@gmail.com>
2016-03-06 19:53:55 +0100

    Projet Unity

---
+++ Assets/_CompletedAssets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+    public class GameOverManager : MonoBehaviour
+    {
+        public PlayerHealth playerHealth;       // Reference to the player's health.
+		public float restartDelay = 5f;
+
+
+        Animator anim;                          // Reference to the animator component.
+		float restartTimer;
+
+        void Awake ()
+        {
+            // Set up the reference.
+            anim = GetComponent <Animator> ();
+        }
+
+
+        void Update ()
+        {
+            // If the player has run out of health...
+            if(playerHealth.currentHealth <= 0)
+            {
+                // ... tell the animator the game is over.
+                anim.SetTrigger ("GameOver");
+				restartTimer += Time.deltaTime;
+				if (restartTimer >= restartDelay) 
+				{
+					Application.LoadLevel (Application.loadedLevel);
+				}
+            }
+        }
+    }
+}
